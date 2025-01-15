5340d13bf2e1c58f45c877df695b9de5a6be5077
Estelle Rostan <estellerostan@pm.me>
2025-01-15 13:45:13 +0100

    Add base project

---
+++ src/shader.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <glad.h>
+
+struct ShaderProgram {
+	GLuint vertShaderId;
+	GLuint fragShaderId;
+	GLuint programId;
+};
+
+struct CreateShaderProgramParams {
+	char const* szVertFilePath;
+	char const* szFragFilePath;
+};
+
+bool createShaderProgram(ShaderProgram& program, const CreateShaderProgramParams& params);
+
+struct ShaderProgram3D : ShaderProgram {
+	GLuint modelLocation;
+	GLuint viewLocation;
+	GLuint projectionLocation;
+	GLuint lightDirLocation;
+	GLuint lightStrengthLocation;
+	GLuint ambientLocation;
+	GLuint specularLocation;
+	GLuint specularPowLocation;
+	GLuint lightingEnabledLocation;
+
+	void	 LoadLocation();
+};
+
+bool createShaderProgram3D(ShaderProgram3D& program);
+
+struct ShaderProgram3D_custom : ShaderProgram3D {
+	GLuint timeLocation;
+	void	 LoadLocation();
+};
+
+bool createShaderProgram3D_custom(ShaderProgram3D_custom& program);
+
+struct ShaderProgram2D : ShaderProgram {
+	GLuint viewportSizeLocation;
+};
+
+bool createShaderProgram2D(ShaderProgram2D& program);
+
